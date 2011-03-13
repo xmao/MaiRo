86b2774e478393c933d9a7a6ac3b748e10b2e092
Xizeng Mao <xizeng.mao@gmail.com>
2011-03-13 15:07:02 -0400

    Updated setting paths.

--- mailrobot/setting.py
+++ mailrobot/setting.py
@@ -8,14 +8,14 @@ def get_home_dir() :
     return os.path.expanduser('~')
 
 def get_conf_dir():
-    d = os.path.join(get_home_dir(), '.grobot')
+    d = os.path.join(get_home_dir(), '.mailrobot')
     if not os.path.isdir(d):
         os.mkdir(d)
         os.mkdir(os.path.join(d, 'emails'))
     return d
 
 def get_conf_file():
-    return yaml.load(open(os.path.join(get_conf_dir(), 'grobotrc.yaml')))
+    return yaml.load(open(os.path.join(get_conf_dir(), 'mailrobotrc.yaml')))
 
 def get_mailbox_dir(mailbox=''):
     d = os.path.join(get_conf_dir(), 'emails', mailbox)
